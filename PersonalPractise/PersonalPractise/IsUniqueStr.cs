989472cce44f10d2560c1491a1fa05b9d1910e59
CrunchyBiscuits <644710228@qq.com>
2020-07-26 12:11:10 +1000

    面试金典1.1 判断字符是否唯一

---
+++ PersonalPractise/PersonalPractise/IsUniqueStr.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonalPractise
+{
+    class IsUniqueStr
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        // 面试金典1.1 https://leetcode-cn.com/problems/is-unique-lcci/submissions/
+        public bool IsUnique(string astr)
+        {
+            //HashSet<char> temp = new HashSet<char>();
+            //foreach(char s in astr.ToCharArray())
+            //{
+            //    if (!temp.Contains(s))
+            //    {
+            //        temp.Add(s);
+            //    }
+            //    else
+            //    {
+            //        return false;
+            //    }
+            //}
+            //return true;
+
+            for(int i = 0; i < astr.Length; i++)
+            {
+                for(int j = i+1; j<astr.Length; j++)
+                {
+                    if(astr[i] == astr[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
