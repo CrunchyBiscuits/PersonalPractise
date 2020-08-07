17ba25adc3e4647931501039391f29df9be87fad
CrunchyBiscuits <644710228@qq.com>
2020-08-07 10:44:47 +1000

    完成2.x练习

--- PersonalPractise/PersonalPractise/LinkedListQuestions.cs
+++ PersonalPractise/PersonalPractise/LinkedListQuestions.cs
@@ -89,10 +89,27 @@ namespace PersonalPractise
         }
 
         // 面试金典2.4 https://leetcode-cn.com/problems/partition-list-lcci/
-        //public ListNode Partition(ListNode head, int x)
-        //{
+        public ListNode Partition(ListNode head, int x)
+        {
+            if (head == null) return null;
+            ListNode shadow = head;
+            while (shadow.next!= null)
+            {
+                if (shadow.next.val < x)
+                {
+                    ListNode temp = shadow.next;
+                    shadow.next = temp.next;
+                    temp.next = head;
+                    head = temp;
+                }
+                else
+                {
+                    shadow = shadow.next;
+                }
+            }
 
-        //}
+            return head;
+        }
 
         // 面试金典2.5 https://leetcode-cn.com/problems/sum-lists-lcci/
         public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
@@ -151,6 +168,48 @@ namespace PersonalPractise
             }
 
             return tail;
+        }
+
+        // 面试金典2.7 https://leetcode-cn.com/problems/intersection-of-two-linked-lists-lcci/
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            ListNode shadow1 = headA;
+            ListNode shadow2 = headB;
+            while (shadow1 != shadow2)
+            {
+                shadow1 = shadow1 != null ? shadow1.next : headB;
+                shadow2 = shadow2 != null ? shadow2.next : headA;
+            }
+            return shadow2;
+        }
+
+        // 面试金典2.8 https://leetcode-cn.com/problems/linked-list-cycle-lcci/
+        public ListNode DetectCycle(ListNode head)
+        {
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while (fast != null && fast.next!=null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+                if(slow == fast)
+                {
+                    break;
+                }
+            }
+
+            if (slow != fast) return null;
+
+            fast = head;
+
+            while (slow != fast)
+            {
+                fast = fast.next;
+                slow = slow.next;
+            }
+
+            return slow;
         }
     }
 }
