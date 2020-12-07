4f5e0b82fd6c1d7b1d8ee4463d092647ebb0948e
zzy <644710228@qq.com>
2020-12-07 19:21:19 +0800

    bilibili1

---
+++ PersonalPractise/PersonalPractise/bilibili.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonalPractise
+{
+    class bilibili
+    {
+        // 20 有效括号
+        public bool IsValid(string s)
+        {
+            if (s == null || s.Length < 1)
+            {
+                return true;
+            }
+            if (s.Length % 2 != 0)
+            {
+                return false;
+            }
+            Dictionary<char, char> helper = new Dictionary<char, char>();
+            
+            helper[')'] = '(';
+            helper[']'] = '[';
+            helper['}'] = '{';
+
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (helper.ContainsKey(s[i]))
+                {
+                    if (stack.Count==0||stack.Peek()!=helper[s[i]])
+                    {
+                        return false;
+                    }
+                    stack.Pop();
+                }
+                else
+                {
+                    stack.Push(s[i]);
+                }
+            }
+            return stack.Count==0;
+        }
+    }
+}
